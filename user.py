aea35b9aac72bc9f4a5442c50d3ce2dd8f8053a5
Eric Malmquist <ericmalmquist7@gmail.com>
2021-01-08 10:16:03 -0700

    Revert "Added Example User Connection to HTML & PyCharm Files"
    This reverts commit a1b938a27e932e6bc39ab896a767271efe6e5d5a.

--- user.py
+++ user.py
@@ -1,69 +1,30 @@
-#!/usr/bin/env python
+import numpy as np
 
-# WS server example that synchronizes state across clients
+# Modes
+# shutdown - no control
+# standby - orient at [0, 0, 0] and await commands
+# trackattitude - track attitude at RPYtarget
+# searchtarget - search for target and lock on
 
-import asyncio
-import json
-import logging
-import websockets
+class User:
 
-logging.basicConfig()
+    def __init__(self):
+        pass
 
-STATE = {"value": 0}
+    mode = "trackattitude"
+    target = [30, 10, 120]
 
-USERS = set()
+    def getLatestCommand(self):
+        return self.mode
 
+    def getLatestTarget(self):
+        return self.target
 
-def state_event():
-    return json.dumps({"type": "state", **STATE})
-
-
-def users_event():
-    return json.dumps({"type": "users", "count": len(USERS)})
-
-
-async def notify_state():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        message = state_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+    def sendData(self, data):
+        #send data
+        #print(data)
+        pass
 
 
-async def notify_users():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        message = users_event()
-        await asyncio.wait([user.send(message) for user in USERS])
 
 
-async def register(websocket):
-    USERS.add(websocket)
-    await notify_users()
-
-
-async def unregister(websocket):
-    USERS.remove(websocket)
-    await notify_users()
-
-
-async def counter(websocket, path):
-    # register(websocket) sends user_event() to websocket
-    await register(websocket)
-    try:
-        await websocket.send(state_event())
-        async for message in websocket:
-            data = json.loads(message)
-            if data["action"] == "minus":
-                STATE["value"] -= 1
-                await notify_state()
-            elif data["action"] == "plus":
-                STATE["value"] += 1
-                await notify_state()
-            else:
-                logging.error("unsupported event: {}", data)
-    finally:
-        await unregister(websocket)
-
-
-start_server = websockets.serve(counter, "localhost", 6789)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
