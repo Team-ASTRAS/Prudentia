5f02ec4cd281d1b5c29359a11074d7fb9981c25d
Rabbittj <rabbittj@my.erau.edu>
2021-02-13 11:28:07 -0700

    All buttons added, Holding features work and account for speed options. Attitude input functions work. Bugs in the script file that require the functionality section associated with a page to be listed first if on that page, otherwise file will stop.

--- rPi Scripts/GUI Files/script.js
+++ rPi Scripts/GUI Files/script.js
@@ -27,7 +27,9 @@ var dataMode = document.querySelector('.dataMode'),
     rollcw = document.querySelector('.RollCW'),
     rollccw = document.querySelector('.RollCCW'),
     // AI BUTTONS
+    go = document.querySelector('.GO'),
     // SM BUTTONS
+    searchmode = document.querySelector('.SearchButton'),
 
 //Connect to RPi here
 ip = "ws://127.0.0.1:8010/";
@@ -85,12 +87,178 @@ clearlog.onclick = function (event) {
 downloadlog.onclick = function (event) {
     console.log('Download Log');
 }
+
 // RTC BUTTONS
 yawl.onmousedown = function (event) {
-    yawl.css('background-color','red');
+    yawl.style.background='red';
+    if(document.getElementById('Speed5').checked){
+      console.log('Yaw Left @ 5 deg/sec');
+    }
+    else if(document.getElementById('Speed10').checked){
+      console.log('Yaw Left @ 10 deg/sec');
+    }
+    else if(document.getElementById('Speed15').checked){
+      console.log('Yaw Left @ 15 deg/sec');
+    }
+    else if(document.getElementById('Speed30').checked){
+      console.log('Yaw Left @ 30 deg/sec');
+    }
+    else if(document.getElementById('Speed50').checked){
+      console.log('Yaw Left @ 50 deg/sec');
+    }
+    else{
+      console.log('Yaw Left @ 20 deg/sec');
+    }
 }
+yawl.onmouseup = function (event) {
+    yawl.style.background='grey';
+    console.log('Not Running')
+}
+
+yawr.onmousedown = function (event) {
+    yawr.style.background='red';
+    if(document.getElementById('Speed5').checked){
+      console.log('Yaw Right @ 5 deg/sec');
+    }
+    else if(document.getElementById('Speed10').checked){
+      console.log('Yaw Right @ 10 deg/sec');
+    }
+    else if(document.getElementById('Speed15').checked){
+      console.log('Yaw Right @ 15 deg/sec');
+    }
+    else if(document.getElementById('Speed30').checked){
+      console.log('Yaw Right @ 30 deg/sec');
+    }
+    else if(document.getElementById('Speed50').checked){
+      console.log('Yaw Right @ 50 deg/sec');
+    }
+    else{
+      console.log('Yaw Right @ 20 deg/sec');
+    }
+}
+yawr.onmouseup = function (event) {
+    yawr.style.background='grey';
+    console.log('Not Running')
+}
+
+pitchu.onmousedown = function (event) {
+    pitchu.style.background='blue';
+    if(document.getElementById('Speed5').checked){
+      console.log('Pitch Up @ 5 deg/sec');
+    }
+    else if(document.getElementById('Speed10').checked){
+      console.log('Pitch Up @ 10 deg/sec');
+    }
+    else if(document.getElementById('Speed15').checked){
+      console.log('Pitch Up @ 15 deg/sec');
+    }
+    else if(document.getElementById('Speed30').checked){
+      console.log('Pitch Up @ 30 deg/sec');
+    }
+    else if(document.getElementById('Speed50').checked){
+      console.log('Pitch Up @ 50 deg/sec');
+    }
+    else{
+      console.log('Pitch Up @ 20 deg/sec');
+    }
+}
+pitchu.onmouseup = function (event) {
+    pitchu.style.background='grey';
+    console.log('Not Running')
+}
+
+pitchd.onmousedown = function (event) {
+    pitchd.style.background='blue';
+    if(document.getElementById('Speed5').checked){
+      console.log('Pitch Down @ 5 deg/sec');
+    }
+    else if(document.getElementById('Speed10').checked){
+      console.log('Pitch Down @ 10 deg/sec');
+    }
+    else if(document.getElementById('Speed15').checked){
+      console.log('Pitch Down @ 15 deg/sec');
+    }
+    else if(document.getElementById('Speed30').checked){
+      console.log('Pitch Down @ 30 deg/sec');
+    }
+    else if(document.getElementById('Speed50').checked){
+      console.log('Pitch Down @ 50 deg/sec');
+    }
+    else{
+      console.log('Pitch Down @ 20 deg/sec');
+    }
+}
+pitchd.onmouseup = function (event) {
+    pitchd.style.background='grey';
+    console.log('Not Running')
+}
+
+rollcw.onmousedown = function (event) {
+    rollcw.style.background='green';
+    if(document.getElementById('Speed5').checked){
+      console.log('Roll CW @ 5 deg/sec');
+    }
+    else if(document.getElementById('Speed10').checked){
+      console.log('Roll CW @ 10 deg/sec');
+    }
+    else if(document.getElementById('Speed15').checked){
+      console.log('Roll CW @ 15 deg/sec');
+    }
+    else if(document.getElementById('Speed30').checked){
+      console.log('Roll CW @ 30 deg/sec');
+    }
+    else if(document.getElementById('Speed50').checked){
+      console.log('Roll CW @ 50 deg/sec');
+    }
+    else{
+      console.log('Roll CW @ 20 deg/sec');
+    }
+}
+rollcw.onmouseup = function (event) {
+    rollcw.style.background='grey';
+    console.log('Not Running')
+}
+
+rollccw.onmousedown = function (event) {
+    rollccw.style.background='green';
+    if(document.getElementById('Speed5').checked){
+      console.log('Roll CCW @ 5 deg/sec');
+    }
+    else if(document.getElementById('Speed10').checked){
+      console.log('Roll CCW @ 10 deg/sec');
+    }
+    else if(document.getElementById('Speed15').checked){
+      console.log('Roll CCW @ 15 deg/sec');
+    }
+    else if(document.getElementById('Speed30').checked){
+      console.log('Roll CCW @ 30 deg/sec');
+    }
+    else if(document.getElementById('Speed50').checked){
+      console.log('Roll CCW @ 50 deg/sec');
+    }
+    else{
+      console.log('Roll CCW @ 20 deg/sec');
+    }
+}
+rollccw.onmouseup = function (event) {
+    rollccw.style.background='grey';
+    console.log('Not Running')
+}
+
 // AI BUTTONS
+go.onclick = function (event) {
+    yawTarget = document.getElementById("YawTarget").value;
+    pitchTarget = document.getElementById("PitchTarget").value;
+    rollTarget = document.getElementById("RollTarget").value;
+    console.log('Target Yaw = ' + yawTarget);
+    console.log('Target Pitch = ' + pitchTarget);
+    console.log('Target Roll = ' + rollTarget);
+}
+
 // SM BUTTONS
+searchmode.onclick = function (event) {
+    console.log('Search Mode');
+}
 
 function setState(state){
     var msg = {"messageType":"setState", "state":state}
